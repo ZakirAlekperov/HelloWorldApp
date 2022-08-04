0b04926e548395dc7ab39610ef9b07b3ad61057d
ZakirAlekperov <zakiralekperov1998@gmail.com>
2022-08-04 23:43:57 +0300

    Приложение из одной кнопки и текстового поля. По нажатию на кнопку, на экране появлется лого андроид и надпись здравствуй мир

--- app/src/main/java/com/zakiralekperov/helloworldapp/MainActivity.java
+++ app/src/main/java/com/zakiralekperov/helloworldapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.zakiralekperov.helloworldapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView helloTextView;
+    private ImageButton startImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloTextView = findViewById(R.id.helloText);
+        startImageButton = findViewById(R.id.startButton);
+    }
+
+
+    public void onStartButtonClick(View view) {
+        startImageButton.setImageResource(R.drawable.android);
+        helloTextView.setVisibility(View.VISIBLE);
+        startImageButton.setClickable(false);
     }
 }
